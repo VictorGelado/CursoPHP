02973ca4835a27fc7c49e1837e01c66bcf4d5ac3
VictorGelado <victorgeladogithub@gmail.com>
2022-02-14 17:28:51 -0300

    start class 10

---
+++ aula10/01/01-exercicio.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="../_css/style.css">
+</head>
+<body>
+<div>
+	<?php 
+		if ((isset($_GET['num']) && $_GET['num'] <> '')) {
+			$num = $_GET['num'];
+		} else {
+			$num = 0;
+		}
+		$tipo = isset($_GET['oper'])? $_GET['oper']: 1;
+
+		switch($tipo) {
+			case 1:
+				$r = $num * 2;
+				break;
+			case 2:
+				$r = pow($num, 3);
+				break;
+			case 3:
+				$r = sqrt($num);
+				break;			
+		}
+
+		echo "<p>O resultado foi: $r</p>";
+	?>
+
+	<a href="./01-exercicio.html"><button>Voltar</button></a>
+
+</div>	
+</body>
+</html>
---
+++ aula10/02/02-exercicio.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="../_css/style.css">
+</head>
+<body>
+<div>
+	<?php 
+		if ((isset($_GET['ds']) && $_GET['ds'] <> '')) {
+			$d = $_GET['ds'];
+		} else {
+			$d = 0;
+		}
+		
+		switch($d) {
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+				echo "Levanta e vai estudar! :)";
+				break;
+			case 7:
+			case 8: 
+				echo "Descanse, pequeno gafanhoto! ;)";
+				break;
+			default:
+				echo "Dia da semana inválido";
+		}
+	?>
+
+	<a href="./02-exercicio.html"><button>Voltar</button></a>
+
+</div>	
+</body>
+</html>
