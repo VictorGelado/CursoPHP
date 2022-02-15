bacbd8fb18309fd643dc76eb36f8fceeacc611bf
VictorGelado <victorgeladogithub@gmail.com>
2022-02-15 15:01:49 -0300

    mv css and start class 11

--- aula03/ola-mundo.php
+++ aula03/ola-mundo.php
@@ -8,7 +8,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Primeiro exemplo PHP</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="../_css/style.css">
 
 	<?php 
 		// echo '<link rel="stylesheet" type="text/css" href="css/style.css" />';
