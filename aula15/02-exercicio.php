b936f47f10566c3c5b020ebd34da98ca4bf0b322
VictorGelado <victorgeladogithub@gmail.com>
2022-02-17 01:41:10 -0300

    finished class 15

---
+++ aula15/02-exercicio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="../_css/style.css">
+</head>
+<body>
+<div>
+	<?php 
+		include "./funcoes.php";
+
+		echo "<h1>Testando novas funcoes</hl>";
+
+		ola();
+
+		mostraValor(3);
+
+		echo "<h2>Finalizando o programa</h2>";
+	?>
+</div>	
+</body>
+</html>
