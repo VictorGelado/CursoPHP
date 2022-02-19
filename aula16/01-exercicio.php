bf37d08d2f182061f9f3ba73d19ec246d54fc98b
VictorGelado <victorgeladogithub@gmail.com>
2022-02-18 21:52:48 -0300

    finished class 16 and 17

---
+++ aula16/01-exercicio.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="../_css/style.css">
+</head>
+<body>
+<div>
+	<?php 
+		$p = "Leite";
+		$pr = 4.5;
+		printf("O %s custa R$ %.2f", $p, $pr);
+		// s = string, f = float
+	?>
+
+</div>	
+</body>
+</html>
---
+++ aula16/02-exercicio.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="../_css/style.css">
+</head>
+<body>
+<div>
+	<?php 
+		$x[0] = 4;
+		$x[1] = 3;
+		$x[2] = 8;
+		// print_r($x);
+
+		var_dump($x);
+		// var_export($x);
+	?>
+
+</div>	
+</body>
+</html>
